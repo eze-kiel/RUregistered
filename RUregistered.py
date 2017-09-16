017b5eeace6e3b8fe5d7d9f0b598f11cf4ffc811
hugo <hugo@localhost.localdomain>
2017-09-16 10:44:33 +0200

    My first commit
    pushing the initial version of the script

---
+++ RUregistered.py
@@ -0,0 +1,71 @@
+import sys
+
+# database sqlite3 pour les pseudos + pswrds -> database permanente
+
+users = {}
+
+def account_check():
+
+    print("")
+    haveAccount = input("do you have an account ? (y/n) ").lower().strip()
+
+    if haveAccount == "y":
+
+        pseudo = input("enter your pseudo: ").lower()
+
+        if pseudo in users:
+
+            password = input("enter your password: ")
+
+            if password in users[pseudo]:
+
+                print("")
+                print("it's ok, welcome {} !".format(pseudo.capitalize()))
+
+            else:
+
+                print("")
+                print("sorry, wrong password !")
+
+        else:
+
+            print("")
+            print("who are you ? :/")
+
+    elif haveAccount == "n":
+
+        print("")
+        choice = input("do you want to register? (y/n) ").lower().strip()
+
+        if choice == "y":
+
+            pseudo = input("enter your pseudo: ").lower()
+
+            if pseudo not in users:
+
+                password = input("enter your password: ")
+                users[pseudo] = password
+                print("")
+                print("you're in the database, welcome !")
+
+            else:
+
+                print("")
+                print("/!\ this pseudo is taken !")
+
+        elif choice == "n":
+
+            print("see ya!")
+            sys.exit()
+
+        else:
+
+            print("unknown command!")
+
+    else:
+
+        print("unknown command!")
+        sys.exit()
+
+while True:
+    account_check()
